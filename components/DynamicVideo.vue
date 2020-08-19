238420fb35c9544c1905250cba34ba0d6dcda697
Carter <greasybutton@gmail.com>
2020-08-19 08:41:55 -0700

    Add global components

---
+++ components/DynamicVideo.vue
@@ -0,0 +1,21 @@
+<template>
+  <video :src="dynamicImage" />
+</template>
+
+<script>
+export default {
+  name: 'DynamicVideo',
+  props: {
+    filename: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    dynamicVideo() {
+      return require(`~/assets/${this.filename}`)
+    }
+  }
+}
+</script>
---
+++ plugins/global.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+import DynamicImage from '~/components/DynamicImage'
+import DynamicVideo from '~/components/DynamicVideo'
+
+Vue.component('dynamic-image', DynamicImage)
+Vue.component('dynamic-video', DynamicVideo)
